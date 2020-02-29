2a354ba8cfe529f9add6159c11fd4c5e39327d34
Panayiotis Lipiridis <lipiridis@gmail.com>
2020-02-29 17:24:59 +0100

    const

--- tasks/download.js
+++ tasks/download.js
@@ -3,17 +3,19 @@
 const https = require("https");
 const fs = require("fs");
 
-const dest = "excalidraw.asar";
-const file = fs.createWriteStream(dest);
+const DEST = "excalidraw.asar";
+const URL = "https://excalidraw.com/excalidraw.asar";
+
+const file = fs.createWriteStream(DEST);
 const request = https
-  .get("https://excalidraw.com/excalidraw.asar", response => {
+  .get(URL, response => {
     response.pipe(file);
     file.on("finish", () => {
-      console.info("excalidraw.asar is downloaded");
+      console.info(`${DEST} is downloaded`);
       file.close();
     });
   })
   .on("error", error => {
-    fs.unlink(dest, () => {});
+    fs.unlink(DEST, () => {});
     console.error(error);
   });
