86ddd8a085f7cc810bab1bb2b1901c390135b036
Shriram Balaji <shrirambalaji1996@gmail.com>
2020-05-26 01:35:18 +0530

    Add timeouts to Spectron App; Enable verbose logging
    * Increase mocha timeout
    * Update electronBinary path

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,15 +1,22 @@
 const Application = require("spectron").Application;
 const assert = require("assert");
-const electronPath = require("electron");
 const path = require("path");
 
+const rootDir = path.resolve(__dirname, "..");
+const isWindows = process.platform === "win32";
+const electronBinary = isWindows ? "electron.cmd" : "electron";
+const electronPath = path.join(rootDir, "node_modules", ".bin", electronBinary);
+
 describe("Application launch", function () {
-  this.timeout(10000);
+  this.timeout(20000);
 
   beforeEach(function () {
     this.app = new Application({
       path: electronPath,
-      args: [path.join(__dirname, "..")],
+      args: [rootDir],
+      startTimeout: 10000,
+      waitTimeout: 10e3,
+      chromeDriverLogPath: "./chromedriver.log",
     });
 
     return this.app.start();
