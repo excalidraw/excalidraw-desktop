6138f9587f0bb80b94cdee2e540cd24fc04d1f78
Niklas Kiefer <niklas.kiefer@camunda.com>
2020-05-26 08:01:26 +0200

    add basic electron test (#46)
    * add basic electron unit test for mac and linux
    
    Co-authored-by: Shriram Balaji <shrirambalaji1996@gmail.com>

---
+++ test/main.spec.js
@@ -0,0 +1,36 @@
+const Application = require("spectron").Application;
+const assert = require("assert");
+const path = require("path");
+
+const rootDir = path.resolve(__dirname, "..");
+const isWindows = process.platform === "win32";
+const electronBinary = isWindows ? "electron.cmd" : "electron";
+const electronPath = path.join(rootDir, "node_modules", ".bin", electronBinary);
+
+describe("Application launch", function () {
+  this.timeout(20000);
+
+  beforeEach(function () {
+    this.app = new Application({
+      path: electronPath,
+      args: [rootDir],
+      startTimeout: 10000,
+      waitTimeout: 10e3,
+      chromeDriverLogPath: "./chromedriver.log",
+    });
+
+    return this.app.start();
+  });
+
+  afterEach(function () {
+    if (this.app && this.app.isRunning()) {
+      return this.app.stop();
+    }
+  });
+
+  it("shows an initial window", function () {
+    return this.app.client.getWindowCount().then(function (count) {
+      assert.equal(count, 1);
+    });
+  });
+});
